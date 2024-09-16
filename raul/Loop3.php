7b5cd958c8a7d601412b1582e41fc75d9c2a6669
RaulCortes08 <166011425+RaulCortes08@users.noreply.github.com>
2024-09-16 12:46:55 +0200

    Add files via upload

---
+++ raul/Loop3.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+        
+       $numero=5;
+       for ($i=1;$i<=$numero;$i++){
+        for($f=1;$f<=$i;$f++){
+           
+            echo $f;
+        }
+            echo "<br>";
+            
+       }
+       
+       
+        
+    ?>
+</body>
+</html>

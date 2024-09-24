94ebb90e72c27ba08c03a5954c1b9c0a80919284
Raul Cortes <166011425+RaulCortes08@users.noreply.github.com>
2024-09-24 13:47:25 +0200

    Scripts22.php

--- raul/Scripts22.php
+++ raul/Scripts22.php
@@ -1,14 +1,13 @@
 <?php
+$numbers = array();
+for ($i = 0; $i < 1000; $i++) {
+    $numbers[] = rand(1, 3000);
+}
 
-$array1 = array fn rand(1, 1000) range(1, 3000);
+$frequency = array_count_values($numbers);
 
-$frequency = array_count_values($array1);
-
-print_r($frequency);    
-
-
-
-
-
+foreach ($frequency as $number => $count) {
+    echo "Number $number appears $count times.\n";
+}
 
 ?>

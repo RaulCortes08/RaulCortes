d77c265acf4394536960f7cc152092e293b12792
Raul Cortes <166011425+RaulCortes08@users.noreply.github.com>
2024-09-26 09:37:03 +0200

    entornos

---
+++ raul/Scripts25.php
@@ -0,0 +1,8 @@
+<?php
+$text = "I was born in the year 1632, in the city of York,
+of a good family, though not of that country, my father being a foreigner of Bremen,
+who settled first at Hull. He got a good estate by merchandise, and leaving off his trade,
+lived afterwards at York, from whence he had married my mother, whose relations were named Robinson";
+$array = explode(" ", $text);
+print_r($array);
+?>
---
+++ raul/Scripts26.php
@@ -0,0 +1,15 @@
+<?php
+$text = array ("I was born in the year 1632, in the city of York,
+of a good family, though not of that country, my father being a foreigner of Bremen,
+who settled first at Hull. He got a good estate by merchandise, and leaving off his trade,
+lived afterwards at York, from whence he had married my mother, whose relations were named Robinson");
+
+$result = array_unique($text);
+print_r($result);
+
+
+
+
+
+
+?>

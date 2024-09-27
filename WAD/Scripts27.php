ba822f5929192ac29fdda23e76557fb32875a455
Raul Cortes <166011425+RaulCortes08@users.noreply.github.com>
2024-09-27 12:01:22 +0200

    .

--- WAD/Scripts27.php
+++ WAD/Scripts27.php
@@ -1,5 +1,8 @@
 <?php
-
+<?php
+$text = "I like to eat bananas with chocolate";
+echo chunk_split($text, 5, "<br>");
+?>
 
 
 
---
+++ WAD/Scripts28.php
@@ -0,0 +1,22 @@
+<?php
+$str = "                                          I like to eat bananas with chocolate, I think I will eat some more later when I am hungry";
+foreach(count_chars($str, 1) as $i => $val) {
+    echo "There were $val instance(s) of \"" , chr($i) , "\" in the string.<br>";
+}
+
+echo "<br><br><br><br>";
+foreach(count_chars(trim($str),1) as $i => $val) {
+    if(chr($i) == " ") {
+        echo "There were $val instance(s) of \"space\" in the string.<br>";
+    }
+}
+echo "<br><br><br><br>";
+echo(str_replace("a", "", $str));
+
+
+
+
+
+
+
+?>
